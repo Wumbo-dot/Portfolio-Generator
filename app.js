c9454f230b3d8961520fa4ceeb6b17a1e607b237
JT Gutierrez <94250514+Wumbo-dot@users.noreply.github.com>
2022-06-22 18:38:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 console.log(profileDataArgs);
+
+const animalArray = ['dog', 'cat', 'pig'];
+
+animalArray.push('cow');
+
+const personObj = {
+  name: 'JT Gutierrez',
+  age: 99
+};
+
+personObj.age = 100;
+personObj.occupation = 'Developer';
